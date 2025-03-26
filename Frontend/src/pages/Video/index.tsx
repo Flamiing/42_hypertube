c280481d2d451b1cda2859c6ac016360f120eeba
magnitopic <magnitopic@gmail.com>
2025-03-26 18:35:21 +0100

    separated video description to it's own component

--- Frontend/src/pages/Video/index.tsx
+++ Frontend/src/pages/Video/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
+import Description from "./Description";
 
 const index: React.FC = () => {
 	const writers = ["Christopher Nolan", "Kai Bird", "Martin Sherwin"];
-
 	const stars = ["Cillian Murphy", "Emily Blunt", "Matt Damon"];
 
 	return (
@@ -11,81 +11,19 @@ const index: React.FC = () => {
 				<div className="flex flex-col gap-4">
 					<div className="">
 						<video
-							className="w-full rounded-lg"
+							className="w-full rounded-lg bg-black"
 							controls
-							src="https://imdb-video.media-imdb.com/vi2053751833/1434659607842-pgv4ql-1683541736696.mp4?Expires=1742370275&Signature=a4Q8x99IIIBdPPKUavgRI3HigowpiBZosRIoj9WCQWZ3RYgvrn8-t-8kLw7bAW-soZ4tsnJLY7GkVN4Y6zDOga5co~GvQNEl2GHiveUBwzohVCQwGd6ozp4wqI4G0FCgVUCiKqGlBsODcoLzvAkpEb9n38BOXmXkCcd2qkoxSGSZMCKF11wRMpYSqg15ODXNaSMtGfDE6ANTIrMxiKY~Vigy9d~H8eGMO2DM3G5v75lxI09QzbPIJV1y-Nc7ECpkSpDgyTW-7ZPWE9TteUXMQu6fikjilEYfG9gBFqWirop9nq68hlDoNduweTw47zyxKD8FoMWdI~cUqzaT84o~2g__&Key-Pair-Id=APKAIFLZBVQZ24NQH3KA"
+							autoplay
+							poster="https://m.media-amazon.com/images/M/MV5BN2JkMDc5MGQtZjg3YS00NmFiLWIyZmQtZTJmNTM5MjVmYTQ4XkEyXkFqcGc@._V1_QL75_UX380_CR0,0,380,562_.jpg"
+							src="https://imdb-video.media-imdb.com/vi2053751833/1434659607842-pgv4ql-1683541736696.mp4?Expires=1742758560&Signature=B458BZsyS3uwMd4vIFW71N1c9WK0EAGdfsVeYjzgDpdzuIGVtbWorMPPjWWrPLMWY9Xa63YUnaIOQNyfWG5lnBKh4BzQMRvwa2UKYJE0JKtl-pv9sYKBSJc-KEwaeHDnBGTGqaCCu6-YJeh8yZLwO3octGokKX0a8FDhnEcu2BBfSrhiFX~A-gR4myrMfTyywgPrlaChBl-Ighrc4EyCiuulYIyRE9QKhEyFkL8hpiEP7~~2~IBE3WA1febIShio02nct0VoDi0bBUvj2ZDxSfxtE4dMlJh52xO44yXROEJg2kLLsWeDcVBvoRBGyZgwGIFexxe-oaMtJnjou10rQw__&Key-Pair-Id=APKAIFLZBVQZ24NQH3KA"
 						></video>
 					</div>
-					<div className="w-full bg-background-secondary p-4 rounded-lg flex flex-col gap-4 mb-7">
-						<div className="relative">
-							<h2 className="text-2xl font-semibold">
-								{"Oppenheimer"}
-								<span> | </span>
-								<span className="text-font-secondary text-xl">
-									{"2023"}
-								</span>
-							</h2>
-							<p>Length: {"3h"}</p>
-							<div className="absolute right-0 top-0 flex items-center flex-col">
-								<p>IMDb rating</p>
-								<p className="text-font-secondary">
-									<i className="fas fa-star text-yellow-400 text-xl pr-2" />
-									<span className="text-font-main">
-										{"8.3"}
-									</span>
-									/{"10"}
-								</p>
-							</div>
-						</div>
-						<div>
-							<div className="flex gap-4">
-								<label htmlFor="" className="text-lg underline">
-									Director
-								</label>
-								<p>{"Christopher Nolan"}</p>
-							</div>
-							<div className="flex gap-4">
-								<label htmlFor="" className="text-lg underline">
-									Writers
-								</label>
-								<p>
-									{writers.map((writer, index) => (
-										<span key={index}>
-											{writer}
-											{index < writers.length - 1 && (
-												<span>, </span>
-											)}
-										</span>
-									))}
-								</p>
-							</div>
-							<div className="flex gap-4">
-								<label htmlFor="" className="text-lg underline">
-									Stars
-								</label>
-								<p>
-									{stars.map((star, index) => (
-										<span key={index}>
-											{star}
-											{index < stars.length - 1 && (
-												<span>, </span>
-											)}
-										</span>
-									))}
-								</p>
-							</div>
-						</div>
-						<div>
-							<label htmlFor="" className="text-lg underline">
-								Summary
-							</label>
-							<p>
-								{
-									"A dramatization of the life story of J. Robert Oppenheimer, the physicist who had a large hand in the development of the atomic bombs that brought an end to World War II."
-								}
-							</p>
-						</div>
-					</div>
+					<Description writers={writers} stars={stars} />
+				</div>
+			</section>
+			<section className="container max-w-4xl mx-auto pt-12 px-4 flex flex-col gap-6">
+				<div className="flex flex-col gap-4 w-full bg-background-secondary p-4 rounded-lg">
+					<h2 className="underline ">Comments</h2>
 				</div>
 			</section>
 		</main>
