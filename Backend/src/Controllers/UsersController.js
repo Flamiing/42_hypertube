da7ad3f609e3a1912058021ec0d98c57b684e566
Amir <amirlaaouam@gmail.com>
2025-03-31 21:24:54 +0200

    feat: profile picture controller implementation (#4)
    This pull request refactors the handling of profile picture
    functionality by moving it from `UsersController` to a new
    `ProfilePictureController`. This change improves code organization and
    separation of concerns. The most important changes include the creation
    of the new controller, updates to the routes, and the removal of the
    profile picture methods from the `UsersController`.
    
    ### Refactoring Profile Picture Handling:
    
    * **Creation of `ProfilePictureController`:**
    - Added a new file `Backend/src/Controllers/ProfilePictureController.js`
    with methods to handle getting, changing, and deleting profile pictures.
    
    * **Updates to `UsersRouter`:**
    - Updated `Backend/src/Routes/UsersRouter.js` to import
    `ProfilePictureController` and use its methods for profile picture
    routes.
    [[1]](diffhunk://#diff-e8b048832fa27b8154508150a35ea25a434b5e1090a19af3e332ed617f29181aR6)
    [[2]](diffhunk://#diff-e8b048832fa27b8154508150a35ea25a434b5e1090a19af3e332ed617f29181aL19-R23)
    [[3]](diffhunk://#diff-e8b048832fa27b8154508150a35ea25a434b5e1090a19af3e332ed617f29181aL34-R38)
    
    * **Removal from `UsersController`:**
    - Removed profile picture handling methods from
    `Backend/src/Controllers/UsersController.js`.
    [[1]](diffhunk://#diff-56f018579659adfeaf94449113cc636c51fe4316620354c4bf5d89439d20323bL89-L109)
    [[2]](diffhunk://#diff-56f018579659adfeaf94449113cc636c51fe4316620354c4bf5d89439d20323bL180-L250)
    - Removed unused imports related to profile picture handling from
    `UsersController`.
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- Backend/src/Controllers/UsersController.js
+++ Backend/src/Controllers/UsersController.js
@@ -1,13 +1,9 @@
-// Third-Party Imports:
-import path from 'path';
-import fsExtra from 'fs-extra';
-
 // Local Imports:
 import userModel from '../Models/UserModel.js';
 import { validatePartialUser } from '../Schemas/userSchema.js';
 import getPublicUser from '../Utils/getPublicUser.js';
 import StatusMessage from '../Utils/StatusMessage.js';
-import { returnErrorWithNext, returnErrorStatus } from '../Utils/errorUtils.js';
+import { returnErrorStatus } from '../Utils/errorUtils.js';
 
 export default class UsersController {
     static async getAllUsers(req, res) {
@@ -86,27 +82,6 @@ export default class UsersController {
         return res.status(500).json({ msg: StatusMessage.QUERY_ERROR });
     }
 
-    static async getProfilePicture(req, res) {
-        const { id } = req.params;
-        const user = await userModel.getById({ id });
-        if (!user)
-            return res.status(500).json({ msg: StatusMessage.QUERY_ERROR });
-        if (user.length === 0)
-            return res.status(404).json({ msg: StatusMessage.USER_NOT_FOUND });
-
-        let profilePicturePath = user.profile_picture;
-        if (!profilePicturePath)
-            profilePicturePath =
-                '/backend/static/images/default-profile-picture.png';
-        const imagePath = path.join(profilePicturePath);
-        res.sendFile(imagePath, (error) => {
-            if (error) {
-                console.error('ERROR:', error);
-                res.status(404).json({ msg: StatusMessage.IMAGE_NOT_FOUND });
-            }
-        });
-    }
-
     static async updateUser(req, res) {
         const isValidData = await UsersController.validateData(req, res);
         if (!isValidData) return res;
@@ -175,77 +150,6 @@ export default class UsersController {
             );
         }
         return { input, inputHasNoContent };
-    }
-
-    static async changeProfilePicture(req, res, next) {
-        const { API_HOST, API_PORT, API_VERSION } = process.env;
-
-        try {
-            const { id } = req.params;
-
-            if (req.files.length !== 1)
-                return returnErrorWithNext(
-                    res,
-                    next,
-                    400,
-                    StatusMessage.BAD_REQUEST
-                );
-
-            const deleteResult =
-                await UsersController.deletePreviousProfilePicture(res, id);
-            if (!deleteResult)
-                return returnErrorWithNext(
-                    res,
-                    next,
-                    res.statusCode,
-                    res.responseData.body
-                );
-
-            const input = { profile_picture: req.files[0].path };
-            const updateResult = await userModel.update({ input, id });
-            if (!updateResult)
-                return returnErrorWithNext(
-                    res,
-                    next,
-                    500,
-                    StatusMessage.INTERNAL_SERVER_ERROR
-                );
-            if (updateResult.length === 0)
-                return returnErrorWithNext(
-                    res,
-                    next,
-                    404,
-                    StatusMessage.USER_NOT_FOUND
-                );
-            return res.json({
-                msg: `http://${API_HOST}:${API_PORT}/api/v${API_VERSION}/users/${id}/profile-picture`,
-            });
-        } catch (error) {
-            console.error('Error uploading file: ', error);
-            return returnErrorWithNext(
-                res,
-                next,
-                400,
-                StatusMessage.ERROR_UPLOADING_IMAGE
-            );
-        }
-    }
-
-    static async deletePreviousProfilePicture(res, id) {
-        const user = await userModel.getById({ id });
-        if (!user)
-            return returnErrorStatus(res, 500, StatusMessage.QUERY_ERROR);
-        if (user.length === 0)
-            return returnErrorStatus(res, 404, StatusMessage.USER_NOT_FOUND);
-        if (!user.profile_picture) return true;
-
-        try {
-            await fsExtra.remove(user.profile_picture);
-            return true;
-        } catch (error) {
-            console.error('Error deleting file: ', error);
-            return false;
-        }
     }
 
     static async getPrivateUser(res, user) {
