0c62f4d5fcc829356c7d3da2f58583f410e39b07
magnitopic <magnitopic@gmail.com>
2025-04-03 00:01:52 +0200

    comments on video page

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,17 +1,14 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import { AuthProvider } from "./context/AuthContext";
-import { SocketProvider } from "./context/SocketContext";
 
 function App() {
 	return (
 		<AuthProvider>
-			<SocketProvider>
-				<RouterProvider
-					future={{ v7_startTransition: true }}
-					router={router}
-				/>
-			</SocketProvider>
+			<RouterProvider
+				future={{ v7_startTransition: true }}
+				router={router}
+			/>
 		</AuthProvider>
 	);
 }
--- Frontend/src/pages/Video/index.tsx
+++ Frontend/src/pages/Video/index.tsx
@@ -1,29 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import Description from "./Description";
+import CommentBubble from "./CommentBubble";
 
 const index: React.FC = () => {
 	const writers = ["Christopher Nolan", "Kai Bird", "Martin Sherwin"];
 	const stars = ["Cillian Murphy", "Emily Blunt", "Matt Damon"];
 
+	const [comments, setComments] = useState([
+		{
+			username: "user1",
+			profilePicture:
+				"https://github.com/magnitopic/matcha/blob/main/Frontend/public/person.png?raw=true",
+			comment: "This is a great movie",
+		},
+	]);
+
+	const handleNewComment = (e) => {
+		e.preventDefault();
+		setComments([
+			{
+				username: "user2",
+				profilePicture:
+					"https://github.com/magnitopic/matcha/blob/main/Frontend/public/person2.png?raw=true",
+				comment: e.target[0].value,
+			},
+			...comments,
+		]);
+		e.target[0].value = "";
+	};
+
 	return (
 		<main className="flex flex-1 justify-center items-center flex-col">
 			<section className="container max-w-4xl mx-auto pt-12 px-4 flex flex-col gap-6">
-				<div className="flex flex-col gap-4">
-					<div className="">
-						<video
-							className="w-full rounded-lg bg-black"
-							controls
-							autoplay
-							poster="https://m.media-amazon.com/images/M/MV5BN2JkMDc5MGQtZjg3YS00NmFiLWIyZmQtZTJmNTM5MjVmYTQ4XkEyXkFqcGc@._V1_QL75_UX380_CR0,0,380,562_.jpg"
-							src="https://imdb-video.media-imdb.com/vi2053751833/1434659607842-pgv4ql-1683541736696.mp4?Expires=1742758560&Signature=B458BZsyS3uwMd4vIFW71N1c9WK0EAGdfsVeYjzgDpdzuIGVtbWorMPPjWWrPLMWY9Xa63YUnaIOQNyfWG5lnBKh4BzQMRvwa2UKYJE0JKtl-pv9sYKBSJc-KEwaeHDnBGTGqaCCu6-YJeh8yZLwO3octGokKX0a8FDhnEcu2BBfSrhiFX~A-gR4myrMfTyywgPrlaChBl-Ighrc4EyCiuulYIyRE9QKhEyFkL8hpiEP7~~2~IBE3WA1febIShio02nct0VoDi0bBUvj2ZDxSfxtE4dMlJh52xO44yXROEJg2kLLsWeDcVBvoRBGyZgwGIFexxe-oaMtJnjou10rQw__&Key-Pair-Id=APKAIFLZBVQZ24NQH3KA"
-						></video>
-					</div>
-					<Description writers={writers} stars={stars} />
-				</div>
+				<video
+					className="w-full rounded-lg bg-black"
+					controls
+					autoPlay
+					src=""
+				></video>
+			</section>
+			<section className="container max-w-4xl mx-auto pt-4 px-4">
+				<Description writers={writers} stars={stars} />
 			</section>
 			<section className="container max-w-4xl mx-auto pt-12 px-4 flex flex-col gap-6">
-				<div className="flex flex-col gap-4 w-full bg-background-secondary p-4 rounded-lg">
-					<h2 className="underline ">Comments</h2>
+				<div className="flex flex-col gap-4 w-full bg-background-secondary p-4 mb-8 rounded-lg">
+					<h2 className="underline text-2xl">Comments</h2>
+					<form
+						className="flex flex-row h-16"
+						onSubmit={handleNewComment}
+					>
+						<input
+							type="text"
+							className="w-full p-4 rounded-l-lg bg-background-primary"
+							placeholder="Add a comment"
+						/>
+						<button
+							type="submit"
+							className="bg-primary text-white rounded-r-lg w-14 h-full flex items-center justify-center disabled:opacity-50 disabled:cursor-not-allowed"
+						>
+							<i className="fa fa-paper-plane" />
+						</button>
+					</form>
+					<div className="flex flex-col gap-4">
+						{comments.map((comment, index) => {
+							return (
+								<CommentBubble comment={comment} key={index} />
+							);
+						})}
+					</div>
 				</div>
 			</section>
 		</main>
