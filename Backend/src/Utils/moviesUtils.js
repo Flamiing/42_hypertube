084abc525f6cfd89fd32aeed3119a3f5e860b4e4
Flamiing <amirlaaouam@gmail.com>
2025-04-18 18:41:00 +0200

    movies are fetched

--- Backend/src/Utils/moviesUtils.js
+++ Backend/src/Utils/moviesUtils.js
@@ -1,4 +1,8 @@
-import { returnErrorStatus } from './errorUtils.js';
+// Third-Party Imports:
+import axios from 'axios';
+
+// Local Imports:
+import { returnErrorStatus } from "./errorUtils.js"
 
 export function isValidSource(res, accepted_sources, source) {
     if (!source)
@@ -10,4 +14,20 @@ export function isValidSource(res, accepted_sources, source) {
     if (!accepted_sources.includes(source.toLowerCase()))
         return returnErrorStatus(res, 400, StatusMessage.INVALID_MOVIES_SOURCE);
     return true;
+}
+
+export async function fetchRawMovies(url) {
+    const rawMovies = []
+
+    try {
+        const response = await axios.get(url);
+        const data = response.data.response.docs;
+        rawMovies.push(...data);
+    } catch (error) {
+        console.error('ERROR:', error)
+        return null;
+    }
+
+    console.log('TEST:', rawMovies)
+    return rawMovies
 }
