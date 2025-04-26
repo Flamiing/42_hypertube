d33c83249c34e00bee01f18abd70e33d6615b0aa
Flamiing <amirlaaouam@gmail.com>
2025-04-26 16:42:21 +0200

    added id and popularity to the library response

--- Backend/src/Controllers/LibraryController.js
+++ Backend/src/Controllers/LibraryController.js
@@ -12,18 +12,16 @@ export default class LibraryController {
         if (isNaN(page))
             return res.status(400).json({ msg: StatusMessage.BAD_REQUEST });
 
-        const fields = ['title', 'year', 'rating', 'thumbnail'];
-        const movies = await moviesModel.getPaginatedRecords(
-            page,
-            6,
-            'popularity',
-            'DESC',
-            fields
-        );
-        if (!movies)
-            return res
-                .status(500)
-                .json({ msg: StatusMessage.INTERNAL_SERVER_ERROR });
+        const fields = [
+            'id',
+            'title',
+            'year',
+            'rating',
+            'thumbnail',
+            'popularity'
+        ];
+        const movies = await moviesModel.getPaginatedRecords(page, 6, 'popularity', 'DESC', fields);
+        if (!movies) return res.status(500).json({ msg: StatusMessage.INTERNAL_SERVER_ERROR })
 
         return res.json({ msg: movies });
     }
